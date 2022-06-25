022cb6a00aa426e4f13b45c27538a3ed2d16871a
Robert Dean <robert@flexbase.app>
2022-06-25 10:16:46 -0400

    updated rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,20 @@
-import dts from 'rollup-plugin-dts'
-import esbuild from 'rollup-plugin-esbuild'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
 
-const name = require('./package.json').main.replace(/\.js$/, '')
-
-const bundle = config => ({
-  ...config,
+export default {
   input: 'src/index.ts',
-  external: id => !/^[./]/.test(id),
-})
+  output: [{
+    file: pkg.main, format: 'cjs',
+  },
+  {
+    file: pkg.module, format: 'es',
+  }],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
+  plugins: [
+    typescript({ typescript: require('typescript'), })
+  ],
+}
 
-export default [
-  bundle({
-    plugins: [esbuild()],
-    output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: `${name}.mjs`,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-  }),
-  bundle({
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: 'es',
-    },
-  }),
-]
